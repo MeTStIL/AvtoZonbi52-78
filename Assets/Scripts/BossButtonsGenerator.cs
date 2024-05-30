9337524d5c9302ee8cad4155fb218865b252e0dd
katushenca <shutenko.katya@bk.ru>
2024-05-31 02:12:19 +0500

    bossRefactoring

--- Assets/Scripts/BossButtonsGenerator.cs
+++ Assets/Scripts/BossButtonsGenerator.cs
@@ -1,16 +1,13 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Fighting;
-using Unity.VisualScripting;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = System.Random;
 using UnityEngine.SceneManagement;
 
-public class Boss_buttons_generator : Sounds
+public class BossButtonsGenerator : Sounds
 {
     [SerializeField] private RawImage bossHealthBar;
     [SerializeField] private RawImage playerHealthBar;
@@ -20,8 +17,7 @@ public class Boss_buttons_generator : Sounds
     public GameObject buttonPrefab;
     private static Dictionary<string, Texture2D> buttonTextures;
     private static string letters = "EQZCFR";
-    private int buttonsLimit = 10;
-    private HashSet<(float, float)> buttonsCoord;
+    private readonly int buttonsLimit = 10;
     private SpriteRenderer sprite;
     private bool isSecondWave;
     private bool isLastWave;
@@ -29,97 +25,120 @@ public class Boss_buttons_generator : Sounds
     private Dictionary<GameObject, KeyCode> buttons;
     private HashSet<GameObject> deletedButtons;
     private int applyButtonsCount;
-    private Vector3 buttonDecreaseKoef;
-    private float buttonVisibilityKoef;
+    private Vector3 buttonDecreaseParameter;
+    private float buttonVisibilityParameter;
     private float buttonsGenTimeDelay;
     private bool isDelay;
     private int buttonsToDamage;
-    private bool isPaused = false;
+    private bool isPaused;
     private ButtonSequenceGen buttonGenerator;
-        
+    private bool isButtonCorrect;
+
     private void Awake()
     {
         buttonGenerator = new ButtonSequenceGen();
         buttonsToDamage = 10;
-        isDelay = false;
         buttonsGenTimeDelay = 0.5f;
-        isSecondWave = false;
-        isLastWave = false;
-        buttonDecreaseKoef = new Vector3(0.003f, 0.003f, 0.003f);
-        buttonVisibilityKoef = 0.0005f;
+        buttonDecreaseParameter = new Vector3(0.003f, 0.003f, 0.003f);
+        buttonVisibilityParameter = 0.0005f;
         applyButtonsCount = 0;   
         timeStart = Time.deltaTime;
         deletedButtons = new HashSet<GameObject>();
         buttons = new Dictionary<GameObject, KeyCode>();
         sprite = GetComponent<SpriteRenderer>();
-        buttonsCoord = new HashSet<(float, float)>();
         bossHeath = 8;
         playerHealth = 8;
         buttonPrefab = Resources.Load<GameObject>("button");
-        if (buttonPrefab == null)
-            Debug.LogError("Не удалось загрузить префаб кнопки!");
-        buttonTextures = Fighting.LettersTo2DTextures.ConnectCharWithTexture(letters);
+        buttonTextures = LettersTo2DTextures.ConnectCharWithTexture(letters);
     }
-    
-    private void Update()
-    {
-        var isButtonCorrect = false;
-        if (applyButtonsCount == buttonsToDamage)
-        {
-            DamageBoss();
-            applyButtonsCount = 0;
-            
-        }
 
-        if (bossHeath > 0 && buttons.Count-deletedButtons.Count < buttonsLimit && timeStart > buttonsGenTimeDelay)
-        {
-            isDelay = false;
-            GenerateButton();
-            timeStart = Time.deltaTime;
-        }
-        
+    private void TryGenerateButton()
+    {
+        if (bossHeath <= 0 || buttons.Count - deletedButtons.Count >= buttonsLimit ||
+            !(timeStart > buttonsGenTimeDelay)) return;
+        isDelay = false;
+        GenerateButton();
+        timeStart = Time.deltaTime;
+    }
+
+    private void CheckForIncorrectClick()
+    {
+        if (!isButtonCorrect && Input.anyKeyDown && !isDelay)
+            DamagePlayer();
+    }
+
+    private bool CheckForCorrectClick(KeyValuePair<GameObject, KeyCode> genButton)
+    {
+        if (!Input.GetKeyDown(genButton.Value) || deletedButtons.Contains(genButton.Key)) return false;
+        isButtonCorrect = true;
+        deletedButtons.Add(genButton.Key);
+        ChangeButtonColor(genButton.Key, genButton.Value, "apply");
+        PlaySound(objectSounds[0], volume: 0.8f, fadeInTime: 0f);
+        applyButtonsCount += 1;
+        Destroy(genButton.Key, 0.3f);
+        return true;
+    }
+
+    private void CheckForUnClicked(KeyValuePair<GameObject, KeyCode> genButton)
+    {
+        if (!(genButton.Key.transform.localScale.x < 2f)) return;
+        deletedButtons.Add(genButton.Key);
+        ChangeButtonColor(genButton.Key, genButton.Value, "cancel");
+        Destroy(genButton.Key, 0.3f);
+        DamagePlayer();
+    }
+
+    private void CheckPlayerInput()
+    {
         foreach (var genButton in buttons)
         {
             if (!deletedButtons.Contains(genButton.Key))
             {
-                if (genButton.Key.transform.localScale.x < 2f)
-                {
-                    deletedButtons.Add(genButton.Key);
-                    ChangeButtonColor(genButton.Key, genButton.Value, "cancel");
-                    Destroy(genButton.Key, 0.3f);
-                    DamagePlayer();
-
-                }
-            }
-            if (Input.GetKeyDown(genButton.Value) && !deletedButtons.Contains(genButton.Key)) 
-            {
-                Debug.Log("ПРАВИЛЬНО НАЖАЛ");
-                
-                isButtonCorrect = true;
-                deletedButtons.Add(genButton.Key);
-                var position = genButton.Key.transform.position;
-                Debug.Log(position);
-
-                ChangeButtonColor(genButton.Key, genButton.Value, "apply");
-                PlaySound(objectSounds[0], volume: 0.8f, fadeInTime: 0f);
-                applyButtonsCount += 1;
-                Destroy(genButton.Key, 0.3f);
-                break;
+                CheckForUnClicked(genButton);
+                if (CheckForCorrectClick(genButton))
+                    break;
+                CheckForIncorrectClick();
             }
         }
-        if (!isButtonCorrect && Input.anyKeyDown && !isDelay)
-            DamagePlayer();
-
-        timeStart += Time.deltaTime;
+        
+    }
+    
+    private void Update()
+    {
+        isButtonCorrect = false;
+        CheckForBossDamage();
+        TryGenerateButton();
+        CheckPlayerInput();
         DecreasingButtons();
+        timeStart += Time.deltaTime;
+        
     }
 
+    private void CheckForBossDamage()
+    {
+        if (applyButtonsCount != buttonsToDamage) return;
+        DamageBoss();
+        applyButtonsCount = 0;
+    }
+    
+    
+
+    private void MakeWaveDelay()
+    {
+        isDelay = true;
+        foreach (var genButton in buttons)
+        {
+            if (deletedButtons.Contains(genButton.Key)) continue;
+            deletedButtons.Add(genButton.Key);
+            Destroy(genButton.Key, 0.3f);
+        }
+        timeStart = -3f;
+    }
+    
     private void DamageBoss()
     {
-        
         bossHeath -= 1;
-        Debug.Log("ЖИЗНЬ БОССА " + bossHeath);
-        var texture = Fighting.LettersTo2DTextures.LoadTextureFromPath($"Assets/{BossType}HealthBar/{bossHeath}.png");
+        var texture = LettersTo2DTextures.LoadTextureFromPath($"Assets/{BossType}HealthBar/{bossHeath}.png");
         bossHealthBar.texture = texture;
         if (bossHeath == 4 && isSecondWave == false)
             MakeSecondWave();
@@ -129,39 +148,21 @@ public class Boss_buttons_generator : Sounds
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         
     }
-
-    private void MakeWaveDelay()
-    {
-        isDelay = true;
-        foreach (var genButton in buttons)
-        {
-            if (!deletedButtons.Contains(genButton.Key))
-            {
-                deletedButtons.Add(genButton.Key);
-                Destroy(genButton.Key, 0.3f);
-
-            }
-        }
-
-        timeStart = -3f;
-    }
     
     private void DamagePlayer()
     {
         PlaySound(objectSounds[3]);
         playerHealth -= 1;
-        Debug.Log("ЖИЗНЬ БОССА " + playerHealth);
-        var texture = Fighting.LettersTo2DTextures.LoadTextureFromPath($"Assets/PlayerHealthBar/{playerHealth}.png");
+        var texture = LettersTo2DTextures.LoadTextureFromPath($"Assets/PlayerHealthBar/{playerHealth}.png");
         playerHealthBar.texture = texture;
         if (playerHealth == 0)
             Death.MoveToScreenDeath();
-        
     }
 
     private void MakeSecondWave()
     {
         PlaySound(objectSounds[1]);
-        buttonDecreaseKoef = new Vector3(0.005f, 0.005f, 0.005f);
+        buttonDecreaseParameter = new Vector3(0.005f, 0.005f, 0.005f);
         isSecondWave = true;
         MakeWaveDelay();
     }
@@ -169,7 +170,7 @@ public class Boss_buttons_generator : Sounds
     private void MakeLastWave()
     {
         PlaySound(objectSounds[2]);
-        buttonDecreaseKoef = new Vector3(0.006f, 0.006f, 0.006f);
+        buttonDecreaseParameter = new Vector3(0.006f, 0.006f, 0.006f);
         isLastWave = true;
         buttonsToDamage = 20;
         buttonsGenTimeDelay = 0.3f;
@@ -186,56 +187,46 @@ public class Boss_buttons_generator : Sounds
     }
     private void DecreasingButtons()
     {
-        foreach (var button in buttons.Keys.Where((k, v) => !deletedButtons.Contains(k)))
+        foreach (var button in buttons.Keys.Where((k, _) => !deletedButtons.Contains(k)))
         {
             isPaused = PauseMenu.isPaused;
-            if (!isPaused)
-            {
-                button.transform.localScale -= buttonDecreaseKoef;
-                var buttonSprite = button.GetComponent<SpriteRenderer>();
-                var color = buttonSprite.color;
-                color.a -= buttonVisibilityKoef;
-                buttonSprite.color = color;
-            }
+            if (isPaused) continue;
+            button.transform.localScale -= buttonDecreaseParameter;
+            var buttonSprite = button.GetComponent<SpriteRenderer>();
+            var color = buttonSprite.color;
+            color.a -= buttonVisibilityParameter;
+            buttonSprite.color = color;
         }
     }
-    
-    public void GenerateButton()
+
+    private void GenerateButton()
     {
-        Debug.Log(buttons.Count - deletedButtons.Count);
-        var koef = 0.3f;
         var buttonSize = 5f;
         var button = buttonGenerator.GenerateButton();
         var random = new Random();
-        Vector3 buttonPosition =sprite.transform.position + new Vector3(random.Next(-5, 5), random.Next(-2, 4), 0);
-        if (!IsCollidesWithButtons(buttonPosition, buttonSize))
-        {
-            buttonsCoord.Add(new(buttonPosition.x, buttonPosition.y));
-            var texture = buttonTextures[button.ToString()];
-            // Создаем экземпляр кнопки из префаба
-            GameObject newButton = Instantiate(buttonPrefab, buttonPosition, Quaternion.identity, transform);
-            SpriteRenderer buttonSpriteRenderer = newButton.GetComponent<SpriteRenderer>();
-            newButton.transform.localScale = new Vector3(buttonSize, buttonSize, buttonSize);
-            buttonSpriteRenderer.sortingOrder = 10;
-            // Устанавливаем текстуру для кнопки
-            buttonSpriteRenderer.sprite =
-                Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
-            buttons[newButton] = ButtonsGenerationInfo.p_Buttons[button];
-        }
+        var buttonPosition =sprite.transform.position + new Vector3(random.Next(-5, 5), 
+            random.Next(-2, 4), 0);
+        if (IsCollidesWithButtons(buttonPosition)) return;
+        var texture = buttonTextures[button.ToString()];
+        var newButton = Instantiate(buttonPrefab, buttonPosition, Quaternion.identity, transform);
+        var buttonSpriteRenderer = newButton.GetComponent<SpriteRenderer>();
+        newButton.transform.localScale = new Vector3(buttonSize, buttonSize, buttonSize);
+        buttonSpriteRenderer.sortingOrder = 10;
+        buttonSpriteRenderer.sprite =
+            Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        buttons[newButton] = ButtonsGenerationInfo.p_Buttons[button];
     }
 
-    private bool IsCollidesWithButtons(Vector3 buttonPosition, float buttonSize)
+    private bool IsCollidesWithButtons(Vector3 buttonPosition)
     {
-        foreach (var button in buttons.Keys.Where((k, v) => !deletedButtons.Contains(k)))
-        {
-            var x = button.transform.position.x;
-            var y = button.transform.position.y;
-            var diffX = Math.Abs(x - buttonPosition.x);
-            var diffY = Math.Abs(y - buttonPosition.y);
-            if (diffX <= 1.5 && diffY < 1.5)
-                return true;
-        }
-
-        return false;
+        return (from button in buttons.Keys
+            .Where((k, _) => !deletedButtons
+                .Contains(k))
+            let x = button.transform.position.x 
+            let y = button.transform.position.y 
+            let diffX = Math.Abs(x - buttonPosition.x) 
+            let diffY = Math.Abs(y - buttonPosition.y) 
+            where diffX <= 1.5 && diffY < 1.5 
+            select diffX).Any();
     }
 }
