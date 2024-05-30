b6952e736aba02091d1189195f846bc62c09c4c3
katushenca <shutenko.katya@bk.ru>
2024-05-30 22:34:31 +0500

    fixing monsterInfo

--- Assets/Scripts/MonsterInfo.cs
+++ Assets/Scripts/MonsterInfo.cs
@@ -1,11 +1,7 @@
-namespace DefaultNamespace
+public class MonsterInfo
 {
-    public class MonsterInfo
-    {
-        public int LivesCount;
-        public int StandardSpeed;
-        public int SpeedAttack;
-        public int ButtonCount;
-        public int CurrentSpeed;
-    }
+    public int LivesCount;
+    public int StandardSpeed;
+    public int SpeedAttack;
+    public int CurrentSpeed;
 }
